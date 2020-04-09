2f11912078e8febfb932e88e6892cea1e9c8096f
TheDeniableToast <filipnorberg02@outlook.com>
2020-04-09 11:18:13 +0200

    Initiated classes

---
+++ src/src/Sprite.java
@@ -0,0 +1,20 @@
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.io.File;
+import java.io.IOException;
+
+public class Sprite {
+
+    private int width;
+    private int height;
+    private int[] pixels;
+
+    public Sprite(int w, int h) {
+        this.width = w;
+        this.height = h;
+        }
+    }
+
+}
